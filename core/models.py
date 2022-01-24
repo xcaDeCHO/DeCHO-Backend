94681db4e29a09102f6e70e9b99df490bcbdc3b7
BdVade <victoraderibigbe03@gmail.com>
2022-01-24 23:57:46 +0100

    migrations and table changes

--- core/models.py
+++ core/models.py
@@ -12,13 +12,13 @@ class Cause(models.Model):
     short_description = models.CharField(max_length=255)
     long_description = models.CharField(max_length=3000)
     status = models.CharField(max_length=12, choices=STATUS, default='pending')
-    wallet_address = models.CharField(max_length=50)
+    wallet_address = models.CharField(max_length=60)
 
 
 class Wallet(models.Model):
     cause = models.OneToOneField(Cause, on_delete=models.CASCADE, related_name="decho_wallet")
     mnemonic = models.CharField(max_length=3000)
-    address = models.CharField(max_length=50)
+    address = models.CharField(max_length=60)
 
 
 class Donation(models.Model):
