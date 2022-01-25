d6c2663dba97f023bb015d75af1ef08a867ac574
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2022-01-25 07:44:26 +0100

    Added view endpoint and .env.example configuration template

--- core/urls.py
+++ core/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create/', views.create_cause, name="createcause"),
+    path('create/', views.create_cause, name="create_cause"),
     path('causes/', views.list_causes, name="list_causes"),
     path('view/<address>', views.check_balances, name="check_balances")
 ]
