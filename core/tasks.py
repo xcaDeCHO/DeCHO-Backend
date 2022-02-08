835d8e6e1b73bc560166a130a5b6b55685cef0a7
Isaac Adewumi <ienioladewumi@gmail.com>
2022-02-08 08:49:48 +0100

    refactor & library swapping

--- core/tasks.py
+++ core/tasks.py
@@ -1,109 +1,113 @@
-from Decho.celery import app
-from .models import Cause, Wallet
-from .utils import check__choice_balance
-from celery import shared_task
-from decouple import config
-from algosdk import account, mnemonic, constants
-from algosdk.v2client import algod
+import logging
+
+from algosdk import account, constants, mnemonic
 from algosdk.future import transaction
+from Decho.settings.common import CHOICE_ID
+from decouple import config
 from django.conf import settings
-from utils import transactions_list
+from huey import crontab
+from huey.contrib.djhuey import db_periodic_task, db_task, task
 
-algod_address = config('ALGOD_ADDRESS')
-algod_token = ''
+from .models import Cause, Wallet
+from .utils import (
+    check_choice_balance,
+    contains_choice_coin,
+    get_transactions,
+    wait_for_transaction_confirmation,
+)
+
+algod_client = settings.ALGOD_CLIENT
+indexer_client = settings.INDEXER_CLIENT
+
+logger = logging.getLogger("huey")
 
 
-@app.task
+@task()
+def test_huey():
+    print("This is just a test")
+
+
+# Two retries might be too much
+@task(retries=2)
+def fund_wallet(wallet_address):
+    logger.info(f"Funding wallet {wallet_address}...")
+
+    suggested_params = algod_client.suggested_params()
+    central_address = config("CENTRAL_WALLET_ADDRESS")
+    unsigned_txn = transaction.PaymentTxn(
+        central_address, suggested_params, wallet_address, 300000, None, ""
+    )
+    signed_txn = unsigned_txn.sign(mnemonic.to_private_key(config("CENTRAL_MNEMONIC")))
+    txid = algod_client.send_transaction(signed_txn)
+
+    wait_for_transaction_confirmation(algod_client, txid)
+    logger.info(f"Funded wallet successfully! {wallet_address}")
+
+    opt_in_to_choice(wallet_address)
+
+
+@db_periodic_task(crontab(minute="*/30"))
 def update_cause_status():
-    causes = Cause.objects.filter(status='pending')
+    logger.info("Attempting to update causes status...")
+
+    causes = Cause.objects.filter(status="pending")
+    logger.info(f"Found {causes.count()} causes pending!")
     for cause in causes:
         address = cause.decho_wallet.address
-        balance = check__choice_balance(address)
+        balance = check_choice_balance(address)
         try:
             balance = int(balance)
         except:
             return
 
         if balance / 100 >= cause.cause_approval.goal:
-            cause.status = 'Approved'
-            transactions = transactions_list(address)
-            for transaction in transactions.get('transactions'):
-                if transaction.get('asset-transfer-transaction').get('reciever') == address:
-                    refund_from_approval(wallet=cause.decho_wallet, reciever=transaction.get('sender'),
-                                         amount=int(transaction.get('asset-transfer-transaction').get('amount')),
-                                         asset=settings.CHOICE_ID)
+            cause.status = "Approved"
+            transactions = get_transactions(indexer_client, address, CHOICE_ID)
+            for _transaction in transactions:
+                refund_from_approval(
+                    wallet=cause.decho_wallet,
+                    reciever=_transaction.get("sender"),
+                    amount=int(_transaction.get("asset-transfer-transaction").get("amount")),
+                    asset=settings.CHOICE_ID,
+                )
 
 
-@app.task
+@db_task(retries=2)
 def opt_in_to_choice(address):
+    logger.info(f"Opting into $CHOICE ASA for {address}...")
+
     wallet = Wallet.objects.get(address=address)
-    print(wallet)
-    client = algod.AlgodClient(algod_token, algod_address)
-    account = client.account_info(address)
-    contains_choice = False
-    for asset in account["assets"]:
-        if asset["asset-id"] == settings.CHOICE_ID:
-            contains_choice = True
-            break
-    if not contains_choice:
-        suggested_params = client.suggested_params()
+    if not contains_choice_coin(algod_client, address):
+        suggested_params = algod_client.suggested_params()
         unsigned_transaction = transaction.AssetTransferTxn(
             wallet.address, suggested_params, wallet.address, 0, settings.CHOICE_ID
         )
         signed_transaction = unsigned_transaction.sign(mnemonic.to_private_key(wallet.mnemonic))
-        transaction_id = client.send_transaction(signed_transaction)
-        contains_choice = True
-    print(contains_choice)
-    return contains_choice
+        transaction_id = algod_client.send_transaction(signed_transaction)
+
+        wait_for_transaction_confirmation(algod_client, transaction_id)
+
+    logger.info(f"Opted into $CHOICE ASA for {address} successful!")
 
 
-@app.task
-def fund_wallet(address):
-    print('fund wallet line 1')
-    client = algod.AlgodClient(algod_token, algod_address)
-    params = client.suggested_params()
-    central_address = config('CENTRAL_WALLET_ADDRESS')
-    unsigned_txn = transaction.PaymentTxn(central_address, params, address, 100000, None, '')
-    signed_txn = unsigned_txn.sign(mnemonic.to_private_key(config('CENTRAL_MNEMONIC')))
-    txid = client.send_transaction(signed_txn)
-    print(txid)
-    try:
-        confirmed_txn = transaction.wait_for_confirmation(client, txid, 4)
-        opt_in_to_choice.delay(address)
-    except Exception as err:
-        print(err)
-        return
-
-
-def refund_from_approval(wallet, reciever, amount, asset):
-    client = algod.AlgodClient(algod_token, algod_address)
-    params = client.suggested_params()
-    txn = transaction.AssetTransferTxn(sender=wallet.address, sp=params, receiver=reciever, amt=amount, index=asset)
+@db_task()
+def refund_from_approval(decho_wallet_addr, reciever_addr, amount, asset):
+    logger.info(
+        f"Refunding approval deposit of {amount} from {decho_wallet_addr} to {reciever_addr}..."
+    )
+    wallet = Wallet.objects.get(address=decho_wallet_addr)
+    suggested_params = algod_client.suggested_params()
+    txn = transaction.AssetTransferTxn(
+        sender=wallet.address,
+        sp=suggested_params,
+        receiver=reciever_addr,
+        amt=amount,
+        index=asset,
+    )
     signed_txn = txn.sign(mnemonic.to_private_key(wallet.mnemonic))
-    client.send_transaction(signed_txn)
+    txn_id = algod_client.send_transaction(signed_txn)
 
-# @app.task
-# def opt_in_to_choice(address):
-#     wallet = Wallet.objects.get(address=address)
-#     client = algod.AlgodClient(algod_token, algod_address)
-#     suggested_params = client.suggested_params()
-#     unsigned_transaction = transaction.AssetTransferTxn(
-#         wallet.address, suggested_params, wallet.address, 0, settings.CHOICE_ID
-#     )
-#     signed_transaction = unsigned_transaction.sign(mnemonic.to_private_key(wallet.mnemonic))
-#     transaction_id = client.send_transaction(signed_transaction)
-
-# from django.db import models
-# from django.template.defaultfilters import slugify
-#
-#
-# class Category(models.Model):
-#     name = models.CharField(max_length=30)
-#     description = models.CharField(max_length=300)
-#
-#
-# class Article(models.Model):
-#     name = models.CharField(max_length=30)
-#     body = models.CharField(max_length=5000)
-#     image = models.ImageField()
-#     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    wait_for_transaction_confirmation(algod_client, txn_id)
+    logger.info(
+        f"Refunded approval deposit of {amount} from {decho_wallet_addr} to {reciever_addr}!"
+    )
--- core/utils.py
+++ core/utils.py
@@ -1,223 +1,65 @@
-import requests
-from rest_framework import status
+from algosdk.v2client.indexer import IndexerClient
 from django.conf import settings
 
+CHOICE_ID = settings.CHOICE_ID
 
 
+def wait_for_transaction_confirmation(algod_client, transaction_id: str):
+    """Wait until the transaction is confirmed or rejected, or until timeout snumber of rounds have passed."""
 
-# from algosdk.v2client import algod
-# from decouple import config
-#
-# testnet_choice_id = config("TESTNET_CHOICE_ID")
-# algod_token = config("TOKEN")
-#
-# algod_address = "https://testnet-algorand.api.purestake.io/ps2"
-# headers = {"X-API-Key": algod_token}
-# algod_client = algod.AlgodClient(algod_token, algod_address, headers)
-#
-#
-# def contains_choice_coin(asa):
-#     account = algod_client.account_info(asa)
-#     contains_choice = False
-#     for asset in account["assets"]:
-#         if asset["asset-id"] == choice_id:
-#             contains_choice = True
-#             break
-#     return contains_choice
-#
-#
-# def check_choice_balance(asa):
-#     if contains_choice_coin(asa):
-#         for asset in algod_client.account_info(asa)["assets"]:
-#             if asset["asset-id"] == choice_id:
-#                 return asset["amount"]
-#     return "ASA do not contain choice"
-#
-#
-# def view(asa):
-#     algo_balance = algod_client.account_info(asa)["amount"]
-#     choice_balance = check_choice_balance(asa)
-#     return {"algo_balance": algo_balance, "choice_balance": choice_balance}
-algo_explorer_address = 'https://algoindexer.testnet.algoexplorerapi.io'
-choice_id = settings.CHOICE_ID
+    TIMEOUT = 5
+    start_round = algod_client.status()["last-round"] + 1
+    current_round = start_round
 
-
-def check__choice_balance(wallet_address):
-    try:
-        response = requests.get(f'{algo_explorer_address}/v2/accounts/{wallet_address}')
-    except Exception as e:
-        print('broke')
-        print(e)
-        return 0
-    print(response.status_code)
-    print(response.json())
-    if response.status_code == 200:
-        response_dict = response.json()
+    while current_round < start_round + TIMEOUT:
         try:
-            for asset in response_dict.get('account').get('assets'):
-                if asset.get('asset-id') == choice_id:
-                    return asset.get('amount') / 100
-                else:
-                    return 'Error: not Opted in'
-        except TypeError:
-            return 'Error: not Opted in'
-    else:
-        print(0)
-        return 0
+            pending_txn = algod_client.pending_transaction_info(transaction_id)
+        except Exception:
+            return
+        if pending_txn.get("confirmed-round", 0) > 0:
+            return pending_txn
+        elif pending_txn["pool-error"]:
+            raise Exception("pool error: {}".format(pending_txn["pool-error"]))
+
+        algod_client.status_after_block(current_round)
+        current_round += 1
+    raise Exception("pending tx not found in TIMEOUT rounds, TIMEOUT value = : {}".format(TIMEOUT))
 
 
-def transactions_list(address):
-    try:
-        response = requests.get(f'{algo_explorer_address}/v2/accounts/{address}/transactions?asset-id=21364625')
-        print(response.json())
-        return response.json()
-    except Exception as e:
-        print('broke')
-        print(e)
+# TODO: You probably want to your opt in error here
+def check_choice_balance(algod_client, address):
+    """Checks if the address is opt into Choice Coin."""
+    account = algod_client.account_info(address)
+    choice_balance = 0
+
+    for asset in account["assets"]:
+        if asset["asset-id"] == CHOICE_ID:
+            choice_balance = asset["amount"]
+            break
+
+    return choice_balance
 
 
+def contains_choice_coin(algod_client, address: str) -> bool:
+    """Checks if the address is opt into Choice Coin."""
+    account = algod_client.account_info(address)
+    contains_choice = False
 
-dict = {
-  "current-round": 19623745,
-  "next-token": "JlUOAQAAAAAEAAAA",
-  "transactions": [
-    {
-      "asset-transfer-transaction": {
-        "amount": 200,
-        "asset-id": 21364625,
-        "close-amount": 0,
-        "receiver": "XAQP4RP2XG47VIZ7HM32MVOPQADXPRJYIQGB3RTMBPFS725TWRLAQXUURM"
-      },
-      "close-rewards": 0,
-      "closing-amount": 0,
-      "confirmed-round": 18806271,
-      "fee": 258000,
-      "first-valid": 18806263,
-      "genesis-hash": "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=",
-      "genesis-id": "testnet-v1.0",
-      "id": "HMJRRQWXORLUFKWCOABCBEXYMZQHN4N6SYJIVJM45YHZSYV74ZKA",
-      "intra-round-offset": 9,
-      "last-valid": 18807263,
-      "receiver-rewards": 0,
-      "round-time": 1640779906,
-      "sender": "NKZA5FMOLRTYPBH2ESM4YSWP3Y5SJDY4OW6ZNJBENS4AESJSO7MW6TBL64",
-      "sender-rewards": 0,
-      "signature": {
-        "sig": "cgzFXhgpGouzVjP8m3AX7Gyxh5cw+fWaFZGCsODMH+cIxWgDfWUDRxq2R1wbZ4tjNxMWesQRoJw6fk4sotHYDw=="
-      },
-      "tx-type": "axfer"
-    },
-    {
-      "asset-transfer-transaction": {
-        "amount": 200,
-        "asset-id": 21364625,
-        "close-amount": 0,
-        "receiver": "XAQP4RP2XG47VIZ7HM32MVOPQADXPRJYIQGB3RTMBPFS725TWRLAQXUURM"
-      },
-      "close-rewards": 0,
-      "closing-amount": 0,
-      "confirmed-round": 18806192,
-      "fee": 258000,
-      "first-valid": 18806184,
-      "genesis-hash": "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=",
-      "genesis-id": "testnet-v1.0",
-      "id": "QTJWQ4WHRXMPCPCTV7DIGVS6JEDJHW4XZTDCT5JEJFRMEMIEJTEA",
-      "intra-round-offset": 9,
-      "last-valid": 18807184,
-      "receiver-rewards": 0,
-      "round-time": 1640779572,
-      "sender": "NKZA5FMOLRTYPBH2ESM4YSWP3Y5SJDY4OW6ZNJBENS4AESJSO7MW6TBL64",
-      "sender-rewards": 0,
-      "signature": {
-        "sig": "PEa+sffnL6iAGlNm8Sr4gwrveSpw1JUXzkME9YDuWkvna7JMVK7To4VV2/WP3kseBuvpJutfa08S/nM/5C97CA=="
-      },
-      "tx-type": "axfer"
-    },
-    {
-      "asset-transfer-transaction": {
-        "amount": 5,
-        "asset-id": 21364625,
-        "close-amount": 0,
-        "receiver": "NKZA5FMOLRTYPBH2ESM4YSWP3Y5SJDY4OW6ZNJBENS4AESJSO7MW6TBL64"
-      },
-      "close-rewards": 0,
-      "closing-amount": 0,
-      "confirmed-round": 17716667,
-      "fee": 1000,
-      "first-valid": 17716539,
-      "genesis-hash": "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=",
-      "genesis-id": "testnet-v1.0",
-      "group": "w0bNtU34T4w4UevgFKTDRr/5xhBLAbZJA4hK/oogZDw=",
-      "id": "DRDLJQMUNDJJZ7UPNDAZDWPCHMVYOW5RU4PUKZYXPUPOVZTKHM5A",
-      "intra-round-offset": 3,
-      "last-valid": 17717539,
-      "receiver-rewards": 0,
-      "round-time": 1636159082,
-      "sender": "KGIWAMCB6C5B2VSVSJQAIHCMGLN6LBBKLUW5PBQ5ZNJ2O5CCMQP2YSPLXM",
-      "sender-rewards": 0,
-      "signature": {
-        "logicsig": {
-          "args": [],
-          "logic": "BCAIAQAAAwSR/5cKBQYhBSQNRDEJMgMSRDEVMgMSRDEgMgMSRDIEIg1EMwEAMQASRDMBECEHEkQzARiB2ZilChJEMwEZIhIzARslEhA3ARoAgAlib290c3RyYXASEEAAXDMBGSMSRDMBG4ECEjcBGgCABHN3YXASEEACEzMBGyISRDcBGgCABG1pbnQSQAE5NwEaAIAEYnVybhJAAYM3ARoAgAZyZWRlZW0SQAIzNwEaAIAEZmVlcxJAAlAAIQYhBCQjEk0yBBJENwEaARchBRI3ARoCFyQSEEQzAgAxABJEMwIQJRJEMwIhIxJEMwIiIxwSRDMCIyEHEkQzAiQjEkQzAiWAB1RNMVBPT0wSRDMCJlEADYANVGlueW1hbiBQb29sIBJEMwIngBNodHRwczovL3RpbnltYW4ub3JnEkQzAikyAxJEMwIqMgMSRDMCKzIDEkQzAiwyAxJEMwMAMQASRDMDECEEEkQzAxEhBRJEMwMUMQASRDMDEiMSRCQjE0AAEDMBATMCAQgzAwEINQFCAYkzBAAxABJEMwQQIQQSRDMEESQSRDMEFDEAEkQzBBIjEkQzAQEzAgEIMwMBCDMEAQg1AUIBVDIEIQYSRDcBHAExABNENwEcATMEFBJEMwIAMQATRDMCFDEAEkQzAwAzAgASRDMDFDMDBzMDECISTTEAEkQzBAAxABJEMwQUMwIAEkQzAQEzBAEINQFCAPwyBCEGEkQ3ARwBMQATRDcBHAEzAhQSRDMDFDMDBzMDECISTTcBHAESRDMCADEAEkQzAhQzBAASRDMDADEAEkQzAxQzAwczAxAiEk0zBAASRDMEADEAE0QzBBQxABJEMwEBMwIBCDMDAQg1AUIAjjIEIQQSRDcBHAExABNEMwIANwEcARJEMwIAMQATRDMDADEAEkQzAhQzAgczAhAiEk0xABJEMwMUMwMHMwMQIhJNMwIAEkQzAQEzAwEINQFCADwyBCUSRDcBHAExABNEMwIUMwIHMwIQIhJNNwEcARJEMwEBMwIBCDUBQgARMgQlEkQzAQEzAgEINQFCAAAzAAAxABNEMwAHMQASRDMACDQBD0M="
-        }
-      },
-      "tx-type": "axfer"
-    },
-    {
-      "asset-transfer-transaction": {
-        "amount": 1036,
-        "asset-id": 21364625,
-        "close-amount": 0,
-        "receiver": "NKZA5FMOLRTYPBH2ESM4YSWP3Y5SJDY4OW6ZNJBENS4AESJSO7MW6TBL64"
-      },
-      "close-rewards": 0,
-      "closing-amount": 0,
-      "confirmed-round": 17716539,
-      "fee": 1000,
-      "first-valid": 17716520,
-      "genesis-hash": "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=",
-      "genesis-id": "testnet-v1.0",
-      "group": "9WCgNYW7HCdBYGiirGlMZTPXLQEDLnjenzTR0qElHgs=",
-      "id": "CY5PNZ6JGMP7DNJBVKVUUOLV5ATORTMTBGPSOFTEN26M7JP646VQ",
-      "intra-round-offset": 6,
-      "last-valid": 17717520,
-      "receiver-rewards": 0,
-      "round-time": 1636158539,
-      "sender": "KGIWAMCB6C5B2VSVSJQAIHCMGLN6LBBKLUW5PBQ5ZNJ2O5CCMQP2YSPLXM",
-      "sender-rewards": 0,
-      "signature": {
-        "logicsig": {
-          "args": [],
-          "logic": "BCAIAQAAAwSR/5cKBQYhBSQNRDEJMgMSRDEVMgMSRDEgMgMSRDIEIg1EMwEAMQASRDMBECEHEkQzARiB2ZilChJEMwEZIhIzARslEhA3ARoAgAlib290c3RyYXASEEAAXDMBGSMSRDMBG4ECEjcBGgCABHN3YXASEEACEzMBGyISRDcBGgCABG1pbnQSQAE5NwEaAIAEYnVybhJAAYM3ARoAgAZyZWRlZW0SQAIzNwEaAIAEZmVlcxJAAlAAIQYhBCQjEk0yBBJENwEaARchBRI3ARoCFyQSEEQzAgAxABJEMwIQJRJEMwIhIxJEMwIiIxwSRDMCIyEHEkQzAiQjEkQzAiWAB1RNMVBPT0wSRDMCJlEADYANVGlueW1hbiBQb29sIBJEMwIngBNodHRwczovL3RpbnltYW4ub3JnEkQzAikyAxJEMwIqMgMSRDMCKzIDEkQzAiwyAxJEMwMAMQASRDMDECEEEkQzAxEhBRJEMwMUMQASRDMDEiMSRCQjE0AAEDMBATMCAQgzAwEINQFCAYkzBAAxABJEMwQQIQQSRDMEESQSRDMEFDEAEkQzBBIjEkQzAQEzAgEIMwMBCDMEAQg1AUIBVDIEIQYSRDcBHAExABNENwEcATMEFBJEMwIAMQATRDMCFDEAEkQzAwAzAgASRDMDFDMDBzMDECISTTEAEkQzBAAxABJEMwQUMwIAEkQzAQEzBAEINQFCAPwyBCEGEkQ3ARwBMQATRDcBHAEzAhQSRDMDFDMDBzMDECISTTcBHAESRDMCADEAEkQzAhQzBAASRDMDADEAEkQzAxQzAwczAxAiEk0zBAASRDMEADEAE0QzBBQxABJEMwEBMwIBCDMDAQg1AUIAjjIEIQQSRDcBHAExABNEMwIANwEcARJEMwIAMQATRDMDADEAEkQzAhQzAgczAhAiEk0xABJEMwMUMwMHMwMQIhJNMwIAEkQzAQEzAwEINQFCADwyBCUSRDcBHAExABNEMwIUMwIHMwIQIhJNNwEcARJEMwEBMwIBCDUBQgARMgQlEkQzAQEzAgEINQFCAAAzAAAxABNEMwAHMQASRDMACDQBD0M="
-        }
-      },
-      "tx-type": "axfer"
-    },
-    {
-      "asset-transfer-transaction": {
-        "amount": 0,
-        "asset-id": 21364625,
-        "close-amount": 0,
-        "receiver": "NKZA5FMOLRTYPBH2ESM4YSWP3Y5SJDY4OW6ZNJBENS4AESJSO7MW6TBL64"
-      },
-      "close-rewards": 0,
-      "closing-amount": 0,
-      "confirmed-round": 17716518,
-      "fee": 1000,
-      "first-valid": 17716458,
-      "genesis-hash": "SGO1GKSzyE7IEPItTxCByw9x8FmnrCDexi9/cOUJOiI=",
-      "genesis-id": "testnet-v1.0",
-      "id": "NC3OQQ33FP3VGZ4KRVFMF6STXQTCQ3CQTZ3MH6WVG7GAHVVQFWGA",
-      "intra-round-offset": 4,
-      "last-valid": 17717458,
-      "receiver-rewards": 0,
-      "round-time": 1636158450,
-      "sender": "NKZA5FMOLRTYPBH2ESM4YSWP3Y5SJDY4OW6ZNJBENS4AESJSO7MW6TBL64",
-      "sender-rewards": 0,
-      "signature": {
-        "sig": "7CNNRPJjqHbJSs3n88+P+aAKDsonwUIHAZs72wp3taEP4hkfoYn3jXhT4gf+lzAOMBzUSI0ZohqrKHyip+PnAw=="
-      },
-      "tx-type": "axfer"
-    }
-  ]
-}
+    for asset in account["assets"]:
+        if asset["asset-id"] == CHOICE_ID:
+            contains_choice = True
+            break
+
+    return contains_choice
+
+
+def get_transactions(indexer_client: IndexerClient, address: str, asa_id: int):
+    """Gets all transactions for a particular ASA on an address"""
+
+    txns = indexer_client.search_asset_transactions(
+        asset_id=asa_id,
+        txn_type="axfer",
+        address=address,
+        address_role="receiver",
+    )
+    return txns["transactions"]
