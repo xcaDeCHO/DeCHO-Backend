16842485ac92ac0721773f6984bc3123b3e1562e
Isaac Adewumi <ienioladewumi@gmail.com>
2022-02-17 15:25:04 +0100

    fix issue with photo_url field

--- core/models.py
+++ core/models.py
@@ -1,23 +1,31 @@
-from django.db import models
+import base64
 from secrets import token_urlsafe
 
+from django.db import models
 
 # Create your models here.
 
 
 class Cause(models.Model):
-    STATUS = (('Approved', 'Approved'),
-              ('pending', 'pending'),
-              ('done', 'done'),
-              ('canceled', 'canceled'),)
+    STATUS = (
+        ("Approved", "Approved"),
+        ("pending", "pending"),
+        ("done", "done"),
+        ("canceled", "canceled"),
+    )
 
     title = models.CharField(max_length=255)
     short_description = models.CharField(max_length=255)
     long_description = models.CharField(max_length=3000)
-    status = models.CharField(max_length=12, choices=STATUS, default='pending')
+    status = models.CharField(max_length=12, choices=STATUS, default="pending")
     wallet_address = models.CharField(max_length=60)
     # uuid = models.CharField(max_length=50, blank=True, editable=False, default=token_urlsafe(15))
-    photo_url = models.URLField(default=f"https://avatars/dicebear.com/api/bottts/{token_urlsafe(15)}")
+    photo_url = models.URLField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if not self.photo_url:
+            self.photo_url = base64.b64encode(str(self.title).encode("ascii")).decode("ascii")
+            super().save(*args, **kwargs)
 
 
 class Wallet(models.Model):
