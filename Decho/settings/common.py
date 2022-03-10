53b2bac16f00a81f05973735262a6c5076a0179d
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2022-03-10 04:11:38 +0100

    Added throttling default policy. Remember to stress test on testnet(dev) before adding throttle views to mainnet(mainnet)

--- Decho/settings/common.py
+++ Decho/settings/common.py
@@ -61,6 +61,14 @@ ROOT_URLCONF = "Decho.urls"
 REST_FRAMEWORK = {
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 10,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '1000/day'
+    }
 }
 
 TEMPLATES = [
