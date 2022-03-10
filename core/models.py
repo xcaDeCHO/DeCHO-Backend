05da0bd8cf279c2271f425a20d979ab3dcc792da
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2022-03-10 04:02:31 +0100

    Altered model and create migrations for new website_uri

--- core/models.py
+++ core/models.py
@@ -1,3 +1,4 @@
+from turtle import Turtle
 from django.db import models
 
 from .utils import gen_random_photo_url
@@ -16,6 +17,7 @@ class Cause(models.Model):
     title = models.CharField(max_length=255)
     long_description = models.CharField(max_length=3000)
     short_description = models.CharField(max_length=255)
+    website_uri = models.URLField(max_length=2048, blank=True, null=True)
     status = models.CharField(max_length=12, choices=STATUS, default="pending")
     wallet_address = models.CharField(max_length=60)
     photo_url = models.URLField(default=gen_random_photo_url)
