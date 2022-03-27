5046aedb87442486a531eeda61d0b22aca37ded7
Isaac Adewumi <ienioladewumi@gmail.com>
2022-03-27 14:38:09 +0100

    chore: change url route name for wallet connect

--- Decho/urls.py
+++ Decho/urls.py
@@ -20,5 +20,5 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("core.urls")),
-    path("connect/", wallet_connect),
+    path("wallet/", wallet_connect),
 ]
