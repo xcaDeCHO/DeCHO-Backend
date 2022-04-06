a9d7fc05aa521c8ab5c387f72794b6009b33593c
Temiloluwa Samuel <65780579+stemitom@users.noreply.github.com>
2022-04-06 12:38:59 +0100

    Commented out throttling

--- Decho/settings/common.py
+++ Decho/settings/common.py
@@ -67,10 +67,10 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle'
     ],
-    'DEFAULT_THROTTLE_RATES': {
-        'anon': '100/day',
-        'user': '1000/day'
-    }
+    # 'DEFAULT_THROTTLE_RATES': {
+    #     'anon': '100/day',
+    #     'user': '1000/day'
+    # }
 }
 
 TEMPLATES = [
