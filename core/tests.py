c70661c279235c5dcf69ed99b9f591d5af67a274
Victor Aderibigbe <victoraderibigbe03@gmail.com>
2022-05-02 13:58:09 +0100

    Update tests.py

--- core/tests.py
+++ core/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
-from molotov import scenario
-
-
-# Create your tests here.
-
-@scenario(weight=100)
-async def _test_local(session):
-    async with session.get("http://localhost:8000/api/v1/causes") as resp:
-        assert resp.status == 200, resp.status
+# from molotov import scenario
+#
+#
+# # Create your tests here.
+#
+# @scenario(weight=100)
+# async def _test_local(session):
+#     async with session.get("http://localhost:8000/api/v1/causes") as resp:
+#         assert resp.status == 200, resp.status
+#
