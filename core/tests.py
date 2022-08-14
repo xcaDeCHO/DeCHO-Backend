9af0cef80f6c80a750b102e4256f6dbd2cea8428
Victor Aderibigbe <victoraderibigbe03@gmail.com>
2022-08-14 15:04:16 +0100

    feat: Add permissions to only allow staff

--- core/tests.py
+++ core/tests.py
@@ -1,9 +1,13 @@
+from decouple import config
 from django.test import TestCase
 from django.conf import settings
-from .utils import decrypt_mnemonic
+from rest_framework.test import APIClient
 
+from .utils import decrypt_mnemonic
+from core.models import Cause
 
 fernet = settings.FERNET
+
 
 # from molotov import scenario
 #
@@ -25,4 +29,36 @@ class TestEncryption(TestCase):
         self.assertEqual(word, decrypted)
 
 
+class TestAPI(TestCase):
 
+    def test_successful_owner_authentication_for_cause_creation(self):
+        client = APIClient()
+        client.credentials(OWNER_TOKEN=config('OWNER'))
+        response = client.post('/api/v1/create/',
+                               data={"cause_approval": {"expiry_date": "2022-05-02T23:45:00.000Z", "goal": 10},
+                                     "donations": {"expiry_date": "2022-05-02T00:00:00.000Z", "goal": 10},
+                                     "title": "First Cause",
+                                     "short_description": "Second cause for test",
+                                     "long_description": "Long long_description gto test",
+                                     "wallet_address": "QT7OZY76NQOIQWRICZNZVNK3QCUUYC4BXB7BBRHXJ3AMHDBARDGAFAXA7I",
+                                     "photo_url": "https://mobile.facebook.com/Football.is.My.Life.HD/photos"
+                                                  "/a.105708091455355/261385459220950/?type=3&_rdc=1&_rdr"},
+                               format='json')
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(Cause.objects.count(), 1)
+
+    def test_failed_owner_authentication_for_cause_creation(self):
+        client = APIClient()
+        client.credentials(OWNER_TOKEN='aa')
+        response = client.post('/api/v1/create/',
+                               data={"cause_approval": {"expiry_date": "2022-05-02T23:45:00.000Z", "goal": 10},
+                                     "donations": {"expiry_date": "2022-05-02T00:00:00.000Z", "goal": 10},
+                                     "title": "First Cause",
+                                     "short_description": "Second cause for test",
+                                     "long_description": "Long long_description gto test",
+                                     "wallet_address": "QT7OZY76NQOIQWRICZNZVNK3QCUUYC4BXB7BBRHXJ3AMHDBARDGAFAXA7I",
+                                     "photo_url": "https://mobile.facebook.com/Football.is.My.Life.HD/photos"
+                                                  "/a.105708091455355/261385459220950/?type=3&_rdc=1&_rdr"},
+                               format='json')
+        self.assertEqual(response.status_code, 403)
+        self.assertEqual(Cause.objects.count(), 0)
